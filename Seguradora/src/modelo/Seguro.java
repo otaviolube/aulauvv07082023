66e9f68b423006b8561abceab10114c4c26630f4
Otávio Lube dos Santos <otaviolube@gmail.com>
2023-08-07 08:40:18 -0300

    Add files via upload

---
+++ Seguradora/src/modelo/Seguro.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Seguro {
+    private float valor;
+    private boolean vigencia;
+    private Cliente proprietario;
+
+    public Seguro(float valor, boolean vigencia, Cliente proprietario) {
+        this.valor = valor;
+        this.vigencia = vigencia;
+        this.proprietario = proprietario;
+    }
+
+    public Cliente getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Cliente proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public boolean isVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(boolean vigencia) {
+        this.vigencia = vigencia;
+    }
+    
+    
+}
